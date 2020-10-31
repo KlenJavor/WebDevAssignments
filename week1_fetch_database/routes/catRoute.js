5ca9791ebc7655991587cba98015f278b44d7a8b
unknown <Patricie.Suppala@gmail.com>
2020-10-31 10:36:26 +0200

    Update routers

--- week1_fetch_database/routes/catRoute.js
+++ week1_fetch_database/routes/catRoute.js
@@ -6,31 +6,11 @@ const router = express.Router();
 const catController = require("../controllers/catController");
 const upload = multer({ dest: "uploads/" });
 
-
-/*
-router.get("/", (req, res) => {
-  res.send(`From this endpoint you can get all cats.`);
-});
-
-router.get("/:id", (req, res) => {
-  console.log("catRoute: http get cat with path param", req.params);
-  res.send(
-    `From this endpoint you can get a cat with an id:  ${req.params.id}`
-  );
-});
-*/
 router.get("/", catController.cat_list_get);
-router.get("/:id", catController.cat_get_by_id);
-
 router.post("/", upload.single("cat"), catController.cat_create);
+
 router.put("/:id", catController.cat_update);
-//router.delete("/:id", catController.cat_delete);
-
-/*router.post("/", upload.single('cat'), (req, res) => {
-  console.log("catRoute", req.body, req.file);
-  res.send('trying to add a cat...')
-});*/
-
-router.post("/", upload.single("cat"), catController.cat_create);
+router.get("/:id", catController.cat_get_by_id);
+router.delete('/:id', catController.cat_delete);
 
 module.exports = router;
---
+++ week1_fetch_database/routes/userRoute.js
@@ -0,0 +1,14 @@
+"use strict";
+// userRoute
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.get("/", userController.user_list_get);
+router.post("/", upload.single("user"), userController.user_create);
+
+router.put("/:id", userController.user_update);
+router.get("/:id", userController.user_get_by_id);
+router.delete('/:id', userController.user_delete);
+
+module.exports = router;
