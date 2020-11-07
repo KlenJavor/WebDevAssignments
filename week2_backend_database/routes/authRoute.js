e454e7834f3ae630370fe74ce5ddaf8c7ac41c79
unknown <Patricie.Suppala@gmail.com>
2020-11-07 10:48:08 +0200

    add passport authenticate

--- week2_backend_database/routes/authRoute.js
+++ week2_backend_database/routes/authRoute.js
@@ -1,8 +1,8 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require("../controllers/authController");
 
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
