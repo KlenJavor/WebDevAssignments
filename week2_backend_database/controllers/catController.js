c68826e8e18dc71d9e1bdea23752be65c0bd1d08
unknown <Patricie.Suppala@gmail.com>
2020-11-13 02:09:59 +0200

    fixes

--- week2_backend_database/controllers/catController.js
+++ week2_backend_database/controllers/catController.js
@@ -1,8 +1,9 @@
 "use strict";
 // catController
 const catModel = require("../models/catModel");
+const validationResult = require("express-validator");
 const resize = require("../utils/resize");
-const imageMeta = require("../utils/imageMeta");
+const getCoordinates = require("../utils/imageMeta");
 
 const cat_list_get = async (req, res) => {
   const cats = await catModel.getAllCats();
@@ -10,11 +11,12 @@ const cat_list_get = async (req, res) => {
 };
 
 const cat_get_by_id = async (req, res) => {
-  const oneCat = await catModel.getCat(req.params.id);
-  res.json(oneCat);
+  console.log("catController: http get cat with path param", req.params);
+  const cat = await catModel.getCat(req.params.id);
+  res.json(cat);
 };
 
-const make_thumbnail = async (req) => {
+const cat_make_thumbnail = async (req) => {
   try {
     const ready = await resize.makeThumbnail(
       { width: 160, height: 160 },
@@ -22,34 +24,45 @@ const make_thumbnail = async (req) => {
       `./thumbnails/${req.file.filename}`
     );
     if (ready) {
-      console.log("make_thumbnail", "ready");
+      console.log("cat_make_thumbnail", ready);
     }
-  } catch (e) {
-    console.error("make_thumbnail", e.message);
-  }
+  } catch (e) {}
 };
 
 const cat_create = async (req, res) => {
-  const coords = await imageMeta
-    .getCoordinates(req.file.path)
-    .catch((e) => console.error("cat_post", e.message));
-  console.log("coords", coords);
-  const result = await catModel.postCat(req, coords);
-  if (result["error"]) res.status(400).json(result);
-  else {
-    await make_thumbnail(req);
-    res.json(result);
+  //here we will create a cat with data comming from req...
+  console.log("catController cat_create", req.body, req.file);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("validation", errors.array());
+    return res.status(400).json({ errors: errors.array() });
   }
+
+  // get gps coordinates from image
+  const coords = await getCoordinates(req.file.path);
+  console.log("coords", coords);
+  req.body.coords = coords;
+
+  const id = await catModel.insertCat(req);
+  const cat = await catModel.getCat(id);
+  res.send(cat);
 };
 
 const cat_update = async (req, res) => {
-  const result = await catModel.updateCat(req);
-  res.json(result);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("validation", errors.array());
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const updateOk = await catModel.updateCat(req);
+  res.json(`{message: "updated... ${updateOk}"}`);
 };
 
 const cat_delete = async (req, res) => {
-  const result = await catModel.deleteCat(req.params.id);
-  res.json(result);
+  console.log("catController: http delete cat with path param", req.params);
+  const cat = await catModel.deleteCat(req.params.id);
+  console.log("cat_delete", cat);
+  res.json(cat);
 };
 
 module.exports = {
@@ -58,4 +71,5 @@ module.exports = {
   cat_create,
   cat_update,
   cat_delete,
+  cat_make_thumbnail,
 };
