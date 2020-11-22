1b65427310754c351fc284a008ead0dcf5847f4c
unknown <Patricie.Suppala@gmail.com>
2020-11-22 10:04:42 +0200

    thumbnail size fix

--- week2_backend_database/utils/resize.js
+++ week2_backend_database/utils/resize.js
@@ -4,7 +4,10 @@ const sharp = require("sharp");
 const makeThumbnail = async (size, file, path) => {
   // size: {width: X, height: Y}
   // file = full path to image (req.file.path), thumbname = filename (req.file.filename)
-  const thumbnail = await sharp(file).resize(size).toFile(path);
+  console.log("makeThumbnail", file, path);
+  const thumbnail = await sharp(file)
+    .resize(size.width, size.height)
+    .toFile(path);
   return thumbnail;
 };
 
